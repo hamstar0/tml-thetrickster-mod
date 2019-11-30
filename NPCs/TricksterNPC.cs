933f2041c759b7142721743f079fd23f75f51d0c
hamstar0 <webutek@gmail.com>
2019-11-30 07:01:09 -0800

    v1.0.0 - Oops

--- NPCs/TricksterNPC.cs
+++ NPCs/TricksterNPC.cs
@@ -135,7 +135,7 @@ namespace TheTrickster.NPCs {
 		////////////////
 
 		public override bool StrikeNPC( ref double damage, int defense, ref float knockback, int hitDirection, ref bool crit ) {
-			if( damage > 0 && this.npc.life > damage ) {
+			if( damage > 0 && this.npc.life > 1 ) {
 				this.RunOnHitAI();
 			}
 			return true;
