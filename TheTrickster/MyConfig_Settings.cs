07f4a4791f2cbb451b8e702cd022678b66863ec8
hamstar0 <webutek@gmail.com>
2021-01-20 20:49:34 -0800

    v1.4.0 - Separated config AI settings into their own code file

---
+++ TheTrickster/MyConfig_Settings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria.ID;
+using Terraria.ModLoader.Config;
+using HamstarHelpers.Helpers.Debug;
+
+
+namespace TheTrickster {
+	public partial class TheTricksterConfig : ModConfig {
+		[Range( 0f, 10f )]
+		[DefaultValue( 0.04f )]
+		[CustomModConfigItem( typeof( MyFloatInputElement ) )]
+		public float SpawnChance { get; set; } = 0.04f;	//0.02?
+
+
+		[Range( 0, 4096 )]
+		[DefaultValue( 96 )]
+		public int MinTileSpawnDistanceAwayFromAnyPreviousDefeat { get; set; } = 96;
+
+
+		//
+
+		[Range( 1, 10000 )]
+		[DefaultValue( 4 )]
+		public int StatInitialLife { get; set; } = 4;
+
+		[Range( 0, 100000 )]
+		[DefaultValue( 1 )]
+		public int StatLifeAddedEachDefeat { get; set; } = 1;
+
+		[Range( 0, 100000 )]
+		[DefaultValue( 15 )]
+		public int StatLifeMax { get; set; } = 15;
+
+		[Range( 0, 999999 )]
+		[DefaultValue( 999999 )]
+		public int StatDefense { get; set; } = 999999;
+
+
+		//
+
+		public ItemDefinition DropsOnDefeat { get; set; } = new ItemDefinition( ItemID.TeleportationPotion );
+
+
+		////
+
+		[Range( -1, 4048 )]
+		[DefaultValue( 64 )]
+		public int TricksterPKEDetectionTileRangeMax { get; set; } = 64;
+	}
+}
---
+++ TheTrickster/MyConfig_Settings_AI.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.ComponentModel;
+using Terraria.ModLoader.Config;
+using HamstarHelpers.Helpers.Debug;
+
+
+namespace TheTrickster {
+	public partial class TheTricksterConfig : ModConfig {
+		[Range( 0, 60 * 60 * 60 )]
+		[DefaultValue( 60 * 180 )]
+		public int TicksUntilFlee { get; set; } = 60 * 180;
+
+
+		[Range( 1, 60 * 60 * 60 )]
+		[DefaultValue( 60 * 2 )]
+		public int IdleDurationTicks { get; set; } = 60 * 2;
+		
+		[Range( 1, 60 * 60 * 60 )]
+		[DefaultValue( 60 * 30 )]
+		public int LurkDurationTicks { get; set; } = 60 * 30;
+
+		[Range( 1, 60 * 60 * 60 )]
+		[DefaultValue( (int)(60f * 5.5f) )]
+		public int AttackDurationTicks { get; set; } = (int)( 60f * 5.5f );
+
+		[Range( 0, 60 * 60 * 60 )]
+		[DefaultValue( 15 )]
+		public int AttackDurationTicksReducedPerDefeat { get; set; } = 15;
+
+		[Range( 1, 60 * 60 * 60 )]
+		[DefaultValue( 60 * 2 )]
+		public int AttackDurationTicksMinimum { get; set; } = 60 * 2;
+
+		[Range( 0, 60 * 60 * 60 )]
+		[DefaultValue( (int)( 60f * 1.5f ) )]
+		public int CooldownDurationTicks { get; set; } = (int)( 60f * 1.5f );
+
+
+		[Range( 4 * 16, 1000 * 16 )]
+		[DefaultValue( 48 * 16 )]
+		public int AttackRadius { get; set; } = 48 * 16;
+
+		[Range( 4 * 16, 1000 * 16 )]
+		[DefaultValue( 16 * 16 )]
+		public int MinDodgeRadius { get; set; } = 16 * 16;
+
+		[Range( 4 * 16, 1000 * 16 )]
+		[DefaultValue( 44 * 16 )]
+		public int MaxDodgeRadius { get; set; } = 44 * 16;
+
+		[Range( 60, 60 * 60 * 60 )]
+		[DefaultValue( 60 * 15 )]
+		public int InvulnTickDuration { get; set; } = 60 * 15;
+
+
+		[Range( 1, 100 )]
+		[DefaultValue( 1 )]
+		public int HitsBeforeBlink { get; set; } = 1;
+
+		[Range( 2, 60 * 60 * 10 )]
+		[DefaultValue( 60 * 10 )]
+		public int TricksterDefensiveBatTickDuration { get; set; } = 60 * 10;
+
+		//[Range( 0, 100 )]
+		//[DefaultValue( 2 )]
+		//public int MaximumNearbyMobsBeforeFleeing { get; set; } = 2;
+
+
+		[Range( 0, 64 * 16 )]
+		[DefaultValue( 8 * 16 )]
+		public int LurkStealRange { get; set; } = 8 * 16;
+	}
+}
