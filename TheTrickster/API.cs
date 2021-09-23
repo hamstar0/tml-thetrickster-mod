e55ccb0832e75ceea452ca9c5e5f51d64ad6cc64
hamstar0 <webutek@gmail.com>
2021-09-23 03:50:21 -0700

    v1.7.3 - Added TheTricksterAPI.GetTricksterDefeatLocations()

---
+++ TheTrickster/API.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+
+namespace TheTrickster {
+	public class TheTricksterAPI {
+		public static ISet<(int tileX, int tileY)> GetTricksterDefeatLocations() {
+			var myworld = ModContent.GetInstance<TheTricksterWorld>();
+
+			return new HashSet<(int, int)>( myworld.TricksterDefeatLocations );
+		}
+	}
+}
